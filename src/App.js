31e7bf740eb18b25d585ed497f9de23513858e00
Aidinidis Lazaros <aidinidis.lazaros@gmail.com>
2019-04-04 14:38:06 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+import "./App.css";
+
+function App() {
+  const [results, setResults] = useState([]);
+
+  function fetchFromApi() {
+    fetch(
+      "https://webservices.mouzenidis.com/AviaWebApi/v1/locations/origins",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          origin_code: "MOW",
+          destination_code: "SKG",
+          direct: 0
+        })
+      }
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => setResults(data))
+      .catch(e => {
+        console.log(e);
+      });
   }
+  async function fetchFromApiAsync() {
+    try {
+      const res = await fetch(
+        "https://webservices.mouzenidis.com/AviaWebApi/v1/locations/origins",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            origin_code: "MOW",
+            destination_code: "SKG",
+            direct: 0
+          })
+        }
+      );
+      const data = await res.json();
+
+      setResults(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <button onClick={fetchFromApi}>Fetch</button>
+      <button onClick={fetchFromApiAsync}>Fetch async</button>
+      <div>
+        {results.map(result => (
+          <div key={result.code}>{result.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
